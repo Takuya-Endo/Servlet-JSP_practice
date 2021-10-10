cbb995ead4d8e0ea9fbe77de5e6efaff323e83fd
Takuya-Endo <above5purple@gmail.com>
2021-10-10 17:05:05 +0900

    [Add]練習5-2_1

---
+++ example/src/main/java/servlet/Exercise_5_2Servlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Exercise_5_2Servlet
+ */
+@WebServlet("/Exercise_5_2Servlet")
+public class Exercise_5_2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+	}
+
+}
