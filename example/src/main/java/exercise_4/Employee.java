0164b750d3109e202db411ca1c31f077885b6fa7
Takuya-Endo <above5purple@gmail.com>
2021-10-10 10:28:05 +0900

    [Add]練習4-1_1

---
+++ example/src/main/java/exercise_4/Employee.java
@@ -0,0 +1,21 @@
+package exercise_4;
+
+public class Employee {
+	
+	private String id;
+	private String name;
+	
+	public Employee(String id, String name) {
+		this.id = id;
+		this.name= name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+}
