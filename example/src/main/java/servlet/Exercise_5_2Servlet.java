c36803f04bd8d210ad02fbe22e73f49755905926
Takuya-Endo <above5purple@gmail.com>
2021-10-11 05:57:55 +0900

    [Update]練習5-2_3

--- example/src/main/java/servlet/Exercise_5_2Servlet.java
+++ example/src/main/java/servlet/Exercise_5_2Servlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,16 +9,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Exercise_5_2Servlet
- */
 @WebServlet("/Exercise_5_2Servlet")
 public class Exercise_5_2Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String qtype = request.getParameter("qtype");
+		String body = request.getParameter("body");
 		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<title>お問い合わせ受付確認</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>お問い合わせ受付確認</h1>");
+		out.println("<p>下記の通りお問い合わせを受け付けました。</p>");
+		out.println("<p>お名前：<br>");
+		out.println(name + "</p>");
+		out.println("<p>お問い合わせの種類：<br>");
+		out.println(qtype + "</p>");
+		out.println("<p>お問い合わせ内容：<br>");
+		out.println(body + "</p>");
+		out.println("</body>");
+		out.println("</html>");
 		
 	}
 
--- example/src/main/webapp/exercise_5_2.jsp
+++ example/src/main/webapp/exercise_5_2.jsp
@@ -9,16 +9,16 @@
 <body>
 	<h1>お問い合わせフォーム</h1>
 	<form action="/example/Exercise_5_2Servlet" method="post">
-		お名前：
-		<input type="text" name="name">
-		お問い合わせの種類：
+		<p>お名前：<br>
+		<input type="text" name="name"></p>
+		<p>お問い合わせの種類：<br>
 		<select name="qtype">
 			<option value="company">会社について</option>
 			<option value="product">製品について</option>
 			<option value="support">アフターサポートについて</option>
-		</select>
-		お問い合わせ内容：
-		<textarea name="body" rows="5" cols="20"></textarea>
+		</select></p>
+		<p>お問い合わせ内容：<br>
+		<textarea name="body" rows="5" cols="20"></textarea></p>
 		<input type="submit" value="送信">
 	</form>
 </body>
