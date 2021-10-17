c36251d6bb9f51c84bce4e46ad52d387f2b6c4a8
Takuya-Endo <above5purple@gmail.com>
2021-10-17 14:31:22 +0900

    [Update]例題7_4

--- example/src/main/java/health/model/HealthCheckLogic.java
+++ example/src/main/java/health/model/HealthCheckLogic.java
@@ -1,5 +1,23 @@
 package health.model;
 
 public class HealthCheckLogic {
+	
+	public String checkBmi(String height, String weight) {
+		int iHeight = Integer.parseInt(height) / 100;
+		int iWeight = Integer.parseInt(weight);
+		String bmi = String.valueOf(iWeight / (iHeight * iHeight));
+		return bmi;
+	}
+	
+	public String judgeBmi(String bmi) {
+		int iBmi = Integer.parseInt(bmi);
+		if (iBmi < 18.5) {
+			return "痩せ型";
+		} else if (iBmi >= 25) {
+			return "肥満";
+		} else {
+			return "普通";
+		}
+	}
 
 }
