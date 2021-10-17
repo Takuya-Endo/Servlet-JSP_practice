25c8a0d0084a84711fbee1c51cd3147d4746b557
Takuya-Endo <above5purple@gmail.com>
2021-10-17 15:21:24 +0900

    [Update]練習7-2_2

---
+++ example/src/main/java/exercise_7/FruitServlet.java
@@ -0,0 +1,26 @@
+package exercise_7;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FruitServlet
+ */
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/exercise_7/fruit.jsp");
+		requestDispatcher.forward(request, response);
+		
+	}
+
+}
---
+++ example/src/main/webapp/WEB-INF/jsp/exercise_7/fruit.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>果物の値段</title>
+</head>
+<body>
+	<h1>果物の値段</h1>
+	<p>の値段は円です。</p>
+</body>
+</html>
