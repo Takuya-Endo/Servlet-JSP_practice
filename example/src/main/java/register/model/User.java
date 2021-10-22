e5b4d0edb000633cee26c63fc51e1ef8641f80c2
Takuya-Endo <above5purple@gmail.com>
2021-10-23 08:38:07 +0900

    [Update]例題8_6

---
+++ example/src/main/java/register/model/User.java
@@ -0,0 +1,28 @@
+package register.model;
+
+public class User {
+	
+	private String id;
+	private String pass;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
