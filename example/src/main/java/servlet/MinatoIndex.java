e237e96f63859b9630f741b12e51730117a5f87d
Takuya-Endo <above5purple@gmail.com>
2021-10-23 15:55:50 +0900

    [Update]例題9_2

--- example/src/main/java/servlet/MinatoIndex.java
+++ example/src/main/java/servlet/MinatoIndex.java
@@ -1,5 +1,41 @@
 package servlet;
 
-public class MinatoIndex {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MinatoIndex")
+public class MinatoIndex extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		
+		
+		
+		
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/minatoIndex.jsp");
+		requestDispatcher.forward(request, response);
+		
+		
+		
+		//request.setAttribute("text", text);
+		//RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/minatoIndex.jsp");
+		//requestDispatcher.forward(request, response);
+		
+		//request.setAttribute("text", text);
+		//ServletContext servletContext = super.getServletContext();
+		//RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/WEB-INF/jsp/minatoIndex.jsp");
+		//requestDispatcher.forward(request, response);
+		
+	}
+	
 }
--- example/src/main/webapp/WEB-INF/jsp/minatoIndex.jsp
+++ example/src/main/webapp/WEB-INF/jsp/minatoIndex.jsp
@@ -1,12 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>湊くんのページ</title>
+	</head>
+	<body>
+		<h1>湊くんのページへようこそ</h1><br>
+		
+		<a href="<%= request.getContextPath() %>/MinatoIndex?action=like">よいね</a>
+		：人　
+		<a href="<%= request.getContextPath() %>/MinatoIndex?action=dislike">よくないね</a>
+		：人
+		
+	</body>
 </html>
