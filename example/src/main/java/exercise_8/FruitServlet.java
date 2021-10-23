e675219bd0d2fdc5b9113f41e9de95ce67c8788d
Takuya-Endo <above5purple@gmail.com>
2021-10-23 15:17:13 +0900

    [Add]練習8-2_1

---
+++ example/src/main/java/exercise_8/FruitServlet.java
@@ -0,0 +1,29 @@
+package exercise_8;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FruitServlet
+ */
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Fruit fruit = new Fruit("いちご", 700);
+		request.setAttribute("fruit", fruit);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/exercise_7/fruit.jsp");
+		requestDispatcher.forward(request, response);
+		
+	}
+
+}
