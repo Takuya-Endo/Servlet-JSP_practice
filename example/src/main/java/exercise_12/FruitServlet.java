eaf3e75cb8ca9f47d75b2f019ce7b8cff7fe5707
Takuya-Endo <above5purple@gmail.com>
2021-11-03 17:11:34 +0900

    [Add]練習12-2_1

---
+++ example/src/main/java/exercise_12/FruitServlet.java
@@ -0,0 +1,32 @@
+package exercise_12;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FruitServlet
+ */
+@WebServlet("/FruitServlet12")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Fruit fruit = new Fruit("いちご", 700);
+		
+		ServletContext servletContext = this.getServletContext();
+		servletContext.setAttribute("fruit", fruit);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/exercise_12/fruit.jsp");
+		requestDispatcher.forward(request, response);
+		
+	}
+
+}
---
+++ example/src/main/webapp/WEB-INF/jsp/exercise_12/fruit.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%@page import="exercise_12.Fruit" %>
+
+<% Fruit fruit = (Fruit) application.getAttribute("fruit"); %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>果物の値段</title>
+</head>
+<body>
+	<h1>果物の値段</h1>
+	<p><%= fruit.getName() %>の値段は<%= fruit.getPrice() %>円です。</p>
+</body>
+</html>
