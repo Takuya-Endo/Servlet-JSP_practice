7981c083eb185ebf7e134377b0d88f473a5d18d8
Takuya-Endo <above5purple@gmail.com>
2021-11-06 16:08:39 +0900

    [Update]例題13_2

--- example/src/main/java/exercise_13/SampleEmployee.java
+++ example/src/main/java/exercise_13/SampleEmployee.java
@@ -1,5 +1,11 @@
 package exercise_13;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SampleEmployee {
 
 	public static void main(String[] args) {
@@ -18,8 +24,36 @@ public class SampleEmployee {
 //		URLの末尾（jdbc:h2:~/以下）を任意のデータベース名にすることで新規作成
 //		ユーザーメイとパスワードは入力不要で、ユーザー名：saのみ
 		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		
-		
+		try {
+			
+			String sql = "";
+			
+			Class.forName("org.h2.Driver");
+			connection = DriverManager.getConnection("jdbc:h2:~/exercise_13");
+			preparedStatement = connection.prepareStatement(sql);
+			resultSet = preparedStatement.executeQuery();
+			
+			
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				resultSet.close();
+				preparedStatement.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		
 
 	}
